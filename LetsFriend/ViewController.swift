845433de094d1b25b6fbd1048cec4a5abd622ec8
KevinKuo <qqqqqqqqq158@yahoo.com.tw>
2019-10-15 21:13:18 +0800

    Initial Commit

---
+++ LetsFriend/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LetsFriend
+//
+//  Created by KUO Chin Wei on 2019/10/15.
+//  Copyright © 2019 KUO Chin Wei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
